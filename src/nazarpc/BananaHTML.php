7986c09aa733a553b9cb7b2007fd00907fc92762
Nazar Mokrynskyi <nazar@mokrynskyi.com>
2014-05-19 06:21:49 +0300

    Wrong version

--- src/nazarpc/BananaHTML.php
+++ src/nazarpc/BananaHTML.php
@@ -1,7 +1,7 @@
 <?php
 /**
  * @package		BananaHTML
- * @version		1.0.2
+ * @version		1.0.3
  * @author		Nazar Mokrynskyi <nazar@mokrynskyi.com>
  * @copyright	Copyright (c) 2011-2014, Nazar Mokrynskyi
  * @license		MIT License, see license.txt
